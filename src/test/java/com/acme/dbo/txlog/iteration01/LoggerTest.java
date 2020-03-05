1f92a48b8c773fd34540ad012ade14d145d37dc3
Vladimir Golubkov <vgol.mail@gmail.com>
2020-03-05 14:18:34 +0300

    Change test1

--- src/test/java/com/acme/dbo/txlog/iteration01/LoggerTest.java
+++ src/test/java/com/acme/dbo/txlog/iteration01/LoggerTest.java
@@ -31,8 +31,10 @@ public class LoggerTest implements SysoutCaptureAndAssertionAbility {
         //endregion
 
         //region then
-        assertSysoutContains("primitive: ");
-        assertSysoutEquals("primitive: 1\nprimitive: 0\nprimitive: -1\n");
+        assertSysoutContains("primitive: 1");
+        assertSysoutContains("primitive: 0");
+        assertSysoutContains("primitive: -1");
+        // assertSysoutEquals("primitive: 1" + System.lineSeparator() +"primitive: 0" + System.lineSeparator()+ "primitive: -1" + System.lineSeparator());
         //endregion
     }
 
